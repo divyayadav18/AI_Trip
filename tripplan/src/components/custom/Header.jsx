486e7c5a330cdae33fc57f06d5ec38c1439a580f
divyayadav18 <dy051622@gmail.com>
2025-07-26 20:21:28 +0530

    updated jsx

--- tripplan/src/components/custom/Header.jsx
+++ tripplan/src/components/custom/Header.jsx
@@ -1,6 +1,6 @@
 {/*Header jsx*/}
 import React, { useState, useEffect } from "react";
-import './Header.css';
+import "./Header.css"
 import SignIn from "./SignIn";
 import ChatBot from "./chatbot";
 
