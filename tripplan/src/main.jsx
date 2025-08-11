880efd017475e0e97205c696b90018d2c793df19
divyayadav18 <dy051622@gmail.com>
2025-08-11 17:13:55 +0530

    updated

--- tripplan/src/main.jsx
+++ tripplan/src/main.jsx
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 
-console.log("Google OAuth Client ID:", import.meta.env.VITE_GOOGLE_AUTH_CLIENT);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
